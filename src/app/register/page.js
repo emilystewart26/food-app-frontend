3961f7b97de5ee706306b95d6e3596ffd32db058
Jeminah <jemasaah@gmail.com>
2025-06-04 14:58:27 +0100

    resolve merge conflict for register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,18 +1,8 @@
-4-create-registration-form
+"use client"; 
+
+import React from 'react';
 import RegisterForm from '../globalComponents/RegisterForm';
 
 export default function RegisterPage() {
   return <RegisterForm />;
 }
-
-"use client";
-import Link from "next/link";
-import React from 'react';
-
-export default function RegisterPage() {
-  return (
-    <div className="h-screen bg-blue-100"> Hello World!
-    </div>
-  );
-}
-main
