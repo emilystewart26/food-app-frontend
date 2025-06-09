aef163ee9b88c0ec33dddf320dbd2dcef8da7185
Ez <ejbennett1@outlook.com>
2025-06-09 14:09:12 +0100

    fix cloudinary upload widget

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React from "react";
 import { Cloudinary } from "@cloudinary/url-gen";
-import { CldUploadWidget } from "next-cloudinary";
+import CloudinaryUploader from "../globalComponents/CloudinaryUploader";
 
 const cld = new Cloudinary({ cloud: { cloudName: process.env.dx9lz1em1 } });
 
@@ -12,19 +12,8 @@ export default function CreateRestaurant() {
       <h1 className="p-4 m-4 text-2xl font-bold text-slate-600">
         Create a page for your restaurant
       </h1>
-      <section>
-        <CldUploadWidget uploadPreset="foodapp_widget">
-          {({ open }) => {
-            return (
-              <button
-                className="flex p-4 bg-transparent hover:bg-slate-600 text-slate-600 hover:text-blue-100 font-bold py-2 px-4 border rounded"
-                onClick={() => open()}>
-                Upload an Image
-              </button>
-            );
-          }}
-        </CldUploadWidget>
-      </section>
+
+      <CloudinaryUploader />
     </div>
   );
 }
