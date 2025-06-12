d196bf4c57504d9db99ff0a2c23d2528d6a7e643
Ez <ejbennett1@outlook.com>
2025-06-12 15:09:22 +0100

    initial code for createrestaurant form

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -2,18 +2,19 @@
 import Link from "next/link";
 import React from "react";
 import { Cloudinary } from "@cloudinary/url-gen";
-import CloudinaryUploader from "../globalComponents/CloudinaryUploader";
+import CreateRestaurant from "../globalComponents/CreateRestaurant";
 
 const cld = new Cloudinary({ cloud: { cloudName: process.env.dx9lz1em1 } });
 
-export default function CreateRestaurant() {
+export default function CreateRestaurantPage() {
   return (
     <div className="flex-row text-center justify-items-center content-start h-screen bg-blue-100 text-black space-x-4">
-      <h1 className="p-4 m-4 text-2xl font-bold text-slate-600">
-        Create a page for your restaurant
-      </h1>
+      {/* <h1 className="p-4 m-4 text-2xl font-bold text-slate-600">
+        Create a page for your establishment
+      </h1> */}
+      <CreateRestaurant />
 
-      <CloudinaryUploader />
+      
     </div>
   );
 }
