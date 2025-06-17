56e87991e94726d1250c5b4f97bef24abd7b68cb
Ez <ejbennett1@outlook.com>
2025-06-17 10:39:12 +0100

    update createrestaurant form

--- src/app/globalComponents/CreateRestaurant.js
+++ src/app/globalComponents/CreateRestaurant.js
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import CloudinaryUploader from "../globalComponents/CloudinaryUploader";
+// import { Checkbox } from './Checkbox';
 
 export default function CreateRestaurant() {
     const [formData, setFormData] = useState({
@@ -35,21 +36,21 @@ export default function CreateRestaurant() {
 
     // Checkbox function???
 
-    function handleInputChange(e) {
-    if (e.target.type == 'checkbox') {
-      if (e.target.checked) {
-        setData({
-          ...data, [e.target.name]: true
-        })
-      } else {
-        setData({
-          ...data, [e.target.name]: false
-        })
-      }
-    } else {
-      setData({ ...data, [e.target.name]: e.target.value })
-    }
-  }
+//     function handleInputChange(e) {
+//     if (e.target.type == 'checkbox') {
+//       if (e.target.checked) {
+//         setData({
+//           ...data, [e.target.name]: true
+//         })
+//       } else {
+//         setData({
+//           ...data, [e.target.name]: false
+//         })
+//       }
+//     } else {
+//       setData({ ...data, [e.target.name]: e.target.value })
+//     }
+//   }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -66,12 +67,6 @@ export default function CreateRestaurant() {
             return;
         }
 
-        if (formData.postcode === '') {
-            setErrorMessage('Please enter a postcode before submitting.');
-            setSuccessMessage('');
-            return;
-        }
-
         if (formData.city === '') {
             setErrorMessage('Please enter a city before submitting.');
             setSuccessMessage('');
@@ -80,12 +75,6 @@ export default function CreateRestaurant() {
 
         if (formData.country === '') {
             setErrorMessage('Please enter a country before submitting.');
-            setSuccessMessage('');
-            return;
-        }
-
-        if (formData.telephone === '') {
-            setErrorMessage('Please enter a telephone number before submitting.');
             setSuccessMessage('');
             return;
         }
@@ -107,11 +96,13 @@ export default function CreateRestaurant() {
         setSuccessMessage('Establishment page submitted!')
     };
 
-    return  (
-        <div className="max-w-md mx-auto mt-24 bg-white p-6 rounded-lg shadow-lg">
-      <h2 className="text-2xl font-bold mb-4 text-center">Create establishment page</h2>
+    //py-10 px-4
 
-      <h3>Details</h3>
+    return  (
+        <div className="max-w-4xl mx-auto mt-24 bg-white p-6 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-10 text-center">Create establishment page</h2>
+
+      {/* <h3 className="block mb-1 font-bold text-xl mb-2">Details</h3> */}
 
       <form onSubmit={handleSubmit} className="space-y-4">
         {/* Name */}
@@ -123,7 +114,7 @@ export default function CreateRestaurant() {
             required
             value={formData.name}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
@@ -136,7 +127,7 @@ export default function CreateRestaurant() {
             required
             value={formData.username}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
@@ -149,7 +140,7 @@ export default function CreateRestaurant() {
             required
             value={formData.postcode}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
@@ -162,7 +153,7 @@ export default function CreateRestaurant() {
             required
             value={formData.city}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
@@ -175,7 +166,7 @@ export default function CreateRestaurant() {
             required
             value={formData.country}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
@@ -188,7 +179,7 @@ export default function CreateRestaurant() {
             required
             value={formData.number}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
@@ -201,18 +192,19 @@ export default function CreateRestaurant() {
             required
             value={formData.website}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="py-2 px-4 w-md border rounded"
             />
         </div>
 
         {/* Category */}
+        {/* Change to checkbox? */}
         <div>
             <label className="block mb-1 font-medium">Category</label>
             <select
             name="category"
             value={formData.category}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="w-md px-4 py-2 border rounded"
             >
                 <option value="">Select a category</option>
                 <option value="cafe">Café</option>
@@ -236,7 +228,7 @@ export default function CreateRestaurant() {
             name="alcohol"
             value={formData.alcohol}
             onChange={handleChange}
-            className="w-full px-4 py-2 border rounded"
+            className="w-md px-4 py-2 border rounded"
             >
                 <option value="">Select an answer</option>
                 <option value="yes">Yes</option>
