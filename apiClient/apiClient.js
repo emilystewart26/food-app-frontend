eb83abc79b25f746b14566203f34cdbecf64d61a
Jeminah <jemasaah@gmail.com>
2025-06-26 15:23:04 +0100

    refactored review codes

--- apiClient/apiClient.js
+++ apiClient/apiClient.js
@@ -1,82 +1,54 @@
 import axios from "axios";
-import { useRouter } from "next/navigation";
 
-const url = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001/api";
+const url = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export class ApiClient {
-  constructor(token = null) {
-    this.token = token;
-
+  constructor() {
     this.axiosInstance = axios.create({
       baseURL: url,
-      withCredentials: true,
+      withCredentials: true, // Required to send Clerk session cookies
     });
-
-    this.axiosInstance.interceptors.request.use(
-      (config) => {
-        if (this.token) {
-          config.headers["Authorization"] = `Bearer ${this.token}`;
-        }
-        return config;
-      },
-      (error) => Promise.reject(error)
-    );
   }
 
-  async apiCall(method, path, data = {}) {
-    console.log (`[apiCall] Method: ${method}, Path: ${path}, Data: ${JSON.stringify(data)}`);
-    console.log (`[apiCall] URL: ${process.env.NEXT_PUBLIC_API_BASE_URL}/${path}`);
-  try {
-    const response = await this.axiosInstance({
-      method,
-      url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/${path}`,
-      data,
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
+  async apiCall(method, path, data = null) {
+    try {
+      const response = await this.axiosInstance({
+        method,
+        url: `/${path}`,
+        data,
+      });
+      return response.data;
+    } catch (error) {
+      console.error(`[ApiClient Error] ${method.toUpperCase()} ${path}`, error);
+      throw error;
+    }
   }
-}
 
+  // RESTAURANT METHODS
 
-  //Restaurant Methods
-  //GET - universtal getRestaurants method by city / with geolocation + with optional additional filters (=queryString)
-  async getRestaurants(queryString) {
-  const path = queryString ? `restaurants?${queryString}` : "restaurants";
-  const response = await this.apiCall("get", path);
-  console.log("[getRestaurants] Query:", queryString);
-  console.log("[getRestaurants] Response:", response);
-  return response;
-}
+  async getRestaurants(queryString = "") {
+    const path = queryString ? `restaurants?${queryString}` : "restaurants";
+    return this.apiCall("get", path);
+  }
 
   async getRestaurantById(id) {
-    const response = await this.apiCall("get", `restaurants/${id}`);
-    return response
+    return this.apiCall("get", `restaurants/${id}`);
   }
 
   async getRestaurantsByUserId(userId) {
-    const response = await this.apiCall("get", `restaurants/user/${userId}`);
-    return response
+    return this.apiCall("get", `restaurants/user/${userId}`);
   }
 
-  //POST
-  async addRestaurant(
-    name,
-    address,
-    city,
-    country /* review Restaurant Schema & check what needs to be listed here*/
-  ) {
+  async addRestaurant(name, address, city, country) {
     return this.apiCall("post", "restaurants", {
       name,
       address,
       city,
       country,
-      // review Restaurant Schema & check if anything else needs to be listed here
     });
   }
 
-  //PUT
-  async updateRestaurant(id, address, postcode, city, country /*as above*/) {
+  async updateRestaurant(id, address, postcode, city, country) {
     return this.apiCall("put", `restaurants/${id}`, {
       address,
       postcode,
@@ -85,79 +57,42 @@ export class ApiClient {
     });
   }
 
-  //DELETE
   async deleteRestaurant(id) {
     return this.apiCall("delete", `restaurants/${id}`);
   }
 
-  //== Review Methods==
+  //  REVIEW METHODS 
 
-  //GET
   async getReviews() {
-    const response = await this.apiCall("get", "reviews");
-    return response
+    return this.apiCall("get", "reviews");
   }
 
   async getReviewsById(id) {
-    const response = await this.apiCall("get", `reviews/${id}`);
-    return response
+    return this.apiCall("get", `reviews/${id}`);
   }
 
   async getReviewsByUserId(userId) {
-    const response = await this.apiCall("get", `reviews/userid/${userId}`);
-    return response
+    return this.apiCall("get", `reviews/userid/${userId}`);
   }
 
   async getReviewsByRestaurantId(restaurantId) {
-    const response = await this.apiCall(
-      "get",
-      `reviews/restaurantid/${restaurantId}`
-    );
-    return response
+    return this.apiCall("get", `reviews/restaurantid/${restaurantId}`);
   }
 
-  // POST
-  async addReview(
-    foodReview,
-    foodStars,
-    ambienceReview,
-    ambienceStars,
-    serviceReview,
-    serviceStars,
-    locationReview,
-    locationStars,
-    userId,
-    restaurantId
-  ) {
-    const response = await this.apiCall("post", "reviews", {
-      foodReview,
-      foodStars,
-      ambienceReview,
-      ambienceStars,
-      serviceReview,
-      serviceStars,
-      locationReview,
-      locationStars,
-      userId,
-      restaurantId,
-    });
-    return response
+  async addReview(data) {
+    return this.apiCall("post", "reviews", data);
   }
 
-  //==Favourites Methods==
+  //  FAVOURITES METHODS
 
-  //GET
   async getFavourites() {
-    const response = await this.apiCall("get", "favourites");
-    return response
+    return this.apiCall("get", "favourites");
   }
 
-  //POST
   async addToFavourites(restaurantId) {
     return this.apiCall("post", `favourites/${restaurantId}`);
   }
 
-  //DELETE
   async removeFromFavourites(restaurantId) {
     return this.apiCall("delete", `favourites/${restaurantId}`);
   }
